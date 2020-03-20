411cc200d5f6014252e820387346e2458b3c1b60
Chang Zheng <mailcz8@gmail.com>
2020-03-20 15:53:01 -0700

    update

--- tests/samples/db_connect.py
+++ tests/samples/db_connect.py
@@ -41,33 +41,56 @@ class SQALite3(object):
         print('insert_entry_local = ', len(args), type(args), args, args[0], args[1], args[2])
         c = conn.cursor()
         # c.execute("INSERT INTO employees VALUES ('King', 'Foot', 50000)")
+        # This is a bad practice because does not check and validate injections
         # c.execute("INSERT INTO employees VALUES({}, {}, {})".format(args[0], args[1], args[2]))
         c.execute("INSERT INTO employees VALUES(?, ?, ?)", (args[0], args[1], args[2]))
         conn.commit()
 
     def insert_entry_remote(self, *args):
-        print(args)
+        # print(args)
         c = conn.cursor()
         # sample: c.execute("INSERT INTO employees VALUES('Mary', 'Lu', 808080)")
         c.execute("INSERT INTO employees VALUES (?, ?, ?)", (args[0], args[1], args[2]))
-        c.execute("INSERT INTO employees VALUES (:first, :last, :pay)", {'first':args[0], 'last':args[1], 'pay':args[2]})
         conn.commit()
 
-    def select_entry(self, query=''):
+    def insert_entry_remote_column_name(self, *args):
+        # print(args)
         c = conn.cursor()
-        c.execute("select * from employees")
-        res = c.fetchone()
-        res_many = c.fetchmany(5)
-        res_all = c.fetchall()
-        print('select_entry =', res_many)
+        # sample: c.execute("INSERT INTO employees VALUES('Mary', 'Lu', 808080)")
+        c.execute("INSERT INTO employees VALUES (:first, :last, :pay)",
+                  {'first': args[0], 'last': args[1], 'pay': args[2]})
         conn.commit()
 
-    def drop_table(self):
+    def select_entry(self, query='', option=3):
         c = conn.cursor()
-        c.execute("DROP TABLE employees")
-        conn.commit()
+        try:
+            c.execute("select * from employees")
+            if option == 1:
+                res = c.fetchone()
+                print("Show one entry =", res)
+            elif option == 2:
+                count = 3
+                res_many = c.fetchmany(count)
+                print('select {} entry ='.format(count), len(res_many), res_many)
+            else:
+                res_all = c.fetchall()
+                for i in range(0,len(res_all)):
+                    print('select all entry of {} = {}'.format(i+1, res_all[i]))
+            conn.commit()
+        except:
+            print("No table found!!!")
+
+    def drop_table(self, table_name='employees'):
+        c = conn.cursor()
+        try:
+            c.execute("DROP TABLE {}".format(table_name))
+            print('{} table dropped.'.format(table_name.upper()))
+            conn.commit()
+        except:
+            print('No table {} found'.format(table_name))
 
     def close_connection(self):
+        print('Close DB connection')
         logging.info('Close DB connection')
         conn.close()
 
@@ -75,19 +98,27 @@ class SQALite3(object):
 if __name__ == "__main__":
     x = SQALite3()
     x.create_emp_table()
-    x.insert_entry_local("King", "Foot", 50000)
-    x.select_entry()
+    x.insert_entry_local("First", "Local", 50000)
+    # x.select_entry()
 
     emp1 = emp("King", "Kong", 555000)
     emp2 = emp("Black", "Pony", 80000)
+    emp3 = emp("Sunday", "School", 80000)
+    emp4 = emp("Pitch", "White", 80000)
+    emp5 = emp("Cookie", "Monster", 80000)
+    emp6 = emp("Last", "End", 80000)
     x.insert_entry_remote(emp1.fname, emp1.lname, emp1.pay)
-    x.insert_entry_remote(emp2.fname, emp2.lname, emp2.pay)
+    x.insert_entry_remote_column_name(emp2.fname, emp2.lname, emp2.pay)
+    x.insert_entry_remote(emp3.fname, emp3.lname, emp3.pay)
+    x.insert_entry_remote(emp4.fname, emp4.lname, emp4.pay)
+    x.insert_entry_remote(emp5.fname, emp3.lname, emp5.pay)
+    x.insert_entry_remote(emp6.fname, emp6.lname, emp6.pay)
     x.select_entry()
     # x.insert_entry_remote(emp2)
     # x.demo_select_entry()
 
-    # x.drop_table()
-    # x.select_entry()
+    x.drop_table()
+    x.select_entry()
     x.close_connection()
 
 
