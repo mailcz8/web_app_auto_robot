6557bca72a463af52071f0180d4e59c6cb074b5f
Chang <mailcz8@gmail.com>
2020-03-29 13:27:38 -0700

    $1

--- my-aliases.bat
+++ my-aliases.bat
@@ -5,6 +5,5 @@ doskey gs=git status
 doskey gb=git branch -a
 doskey gd='git diff'
 doskey gbp='git remote prune origin'
-doskey gitzap='git add . && git commit -m "update" && git push'
-doskey pecho='echo ${*}'
+doskey gitzap=git add . && git commit -m "update" && git push
 echo [Done]
