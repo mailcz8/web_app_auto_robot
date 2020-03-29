edb03c91f29dd670d786f2b5aefc478a4c1a6963
Chang <mailcz8@gmail.com>
2020-03-29 13:44:08 -0700

    !!!Commit all file and changes blindly!!!

--- my-aliases.bat
+++ my-aliases.bat
@@ -6,7 +6,7 @@ doskey gb=git branch -a
 doskey gd='git diff'
 doskey gl=git log --decorate=short
 doskey gbp='git remote prune origin'
-doskey gitzap=git add . && git commit -m "commit all file and changes blindly!!!" && git push
+doskey gitzap=git add . && git commit -m "!!!Commit all file and changes blindly!!!" && git push &&
 doskey pecho=echo $1
-echo change 2
+echo change 4
 echo [Done]
