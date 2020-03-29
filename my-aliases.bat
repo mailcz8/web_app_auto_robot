b267c7e6e1bd61950d64245e66d990ba2c098c0e
Chang <mailcz8@gmail.com>
2020-03-29 13:18:39 -0700

    hello

---
+++ my-aliases.bat
@@ -0,0 +1,11 @@
+@echo on
+doskey aliases=doskey /macros
+doskey ll=dir/w
+doskey gs=git status
+doskey gb=git branch -a
+doskey gd='git diff'
+doskey gbp='git remote prune origin'
+echo   --------------------
+echo   aliases ^=^> list  all
+echo   alt+F10 ^=^> clear all
+echo [Done]
